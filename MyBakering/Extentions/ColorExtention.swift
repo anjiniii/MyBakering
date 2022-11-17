734e71747a8ac58ecaa4b28571453b169ba5d37e
anjin <anjin720jin@naver.com>
2022-11-17 14:16:19 +0900

    Add myColor to Assets and Extensions

---
+++ MyBakering/Extentions/ColorExtention.swift
@@ -0,0 +1,16 @@
+//
+//  ColorExtention.swift
+//  MyBakering
+//
+//  Created by 이안진 on 2022/11/17.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let myLightBrown = Color("myLightBrown")
+    static let myBrown = Color("myBrown")
+    static let myDarkBrown = Color("myDarkBrown")
+    static let myGray = Color("myGray")
+}
