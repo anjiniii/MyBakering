9b7bf8f1436f0405efeb11f0e3dd678edf725501
anjin <anjin720jin@naver.com>
2022-11-18 18:31:19 +0900

    add button at New Recipe View

--- MyBakering/Main/NewRecipe/NewRecipeView.swift
+++ MyBakering/Main/NewRecipe/NewRecipeView.swift
@@ -57,18 +57,21 @@ struct NewRecipeView: View {
                 Section {
                     Text("재료 추가하기")
                         .font(.headline)
-                        .listRowBackground(Color.myLightBrown)
+                        .listRowBackground(Color.accentColor)
                         .foregroundColor(.white)
                     TextField("재료", text: $newIngredient)
-                    HStack {
-                        TextField("양", text: $newVolume)
-                        Divider()
-                        Button {
-                            addIngredient(ingredient: newIngredient, volume: newVolume)
-                        } label: {
-                            Image(systemName: "plus.circle.fill")
+                    TextField("양", text: $newVolume)
+                    Button {
+                        addIngredient(ingredient: newIngredient, volume: newVolume)
+                    } label: {
+                        HStack {
+                            Spacer()
+                            Text("추가하기")
+                            Spacer()
                         }
+                        .foregroundColor(.white).bold()
                     }
+                    .listRowBackground(Color.myLightBrown)
                 } header: {
                     Text("'Edit' 버튼을 눌러 재료와 과정을 수정할 수 있습니다.")
                 }
@@ -91,19 +94,24 @@ struct NewRecipeView: View {
                 Section {
                     Text("과정 추가하기")
                         .font(.headline)
-                        .listRowBackground(Color.myLightBrown)
+                        .listRowBackground(Color.accentColor)
                         .foregroundColor(.white)
                     HStack {
                         Image(systemName: "\(steps.count + 1).square.fill")
                             .foregroundColor(Color.myLightBrown)
                         TextField("과정", text: $newStep, axis: .vertical)
-                        Divider()
-                        Button {
-                            addStep(step: newStep)
-                        } label: {
-                            Image(systemName: "plus.circle.fill")
-                        }
                     }
+                    Button {
+                        addStep(step: newStep)
+                    } label: {
+                        HStack {
+                            Spacer()
+                            Text("추가하기")
+                            Spacer()
+                        }
+                        .foregroundColor(.white).bold()
+                    }
+                    .listRowBackground(Color.myLightBrown)
                 }
                 
                 // 과정
@@ -127,7 +135,7 @@ struct NewRecipeView: View {
                     } label: {
                         HStack {
                             Spacer()
-                            Text("추가하기")
+                            Text("레시피 저장")
                             Spacer()
                         }
                         .foregroundColor(.white).bold()
